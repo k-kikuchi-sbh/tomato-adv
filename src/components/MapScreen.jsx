5f492505c620b3c907c26b354df0579d65de1451
k-kikuchi-sbh <k-kikuchi@sbh-sendai.com>
2025-07-11 17:39:36 +0900

    Update MapScreen.jsx

--- src/components/MapScreen.jsx
+++ src/components/MapScreen.jsx
@@ -2,9 +2,9 @@ export default function MapScreen({ onSelectArea, onGoToChoice }) {
   return (
     <div style={{ textAlign: 'center' }}>
       <h2>どこを調査する？</h2>
-      <button onClick={() => onSelectArea('畑')}>畑</button>
-      <button onClick={() => onSelectArea('市場')}>市場</button>
-      <button onClick={() => onSelectArea('厨房')}>厨房</button>
+      <button className="button-cute" onClick={() => onSelectArea('畑')}>畑</button>
+      <button className="button-cute" onClick={() => onSelectArea('市場')}>市場</button>
+      <button className="button-cute" onClick={() => onSelectArea('厨房')}>厨房</button>
 
       {onGoToChoice && (
         <div style={{ marginTop: '2em' }}>
